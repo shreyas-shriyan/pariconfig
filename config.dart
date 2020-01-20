4096fc6bf24c38e1a5e3577ca23b31e7ce24095a
SHREYAS SHRIYAN <51715896+shreyas-shriyan@users.noreply.github.com>
2020-01-20 20:49:37 +0530

    Add files via upload

--- config.dart
+++ config.dart
@@ -4,14 +4,13 @@ import '../common/constants.dart';
 /// Server config
 const serverConfig = {
   "type": "woo",
-  "url": "http://demo.mstore.io",
-  "consumerKey": "ck_b7594bc4391db4b56c635fe6da1072a53ca4535a",
-  "consumerSecret": "cs_980b9edb120e15bd2a8b668cacc734f7eca0ba40",
+  "url": "https://pariflorist.com",
+  "consumerKey": "ck_99ffd6023ea9aa77e85c9009e1fb36ff91db0c1f",
+  "consumerSecret": "cs_94d0328831557c0107e789bffd16c29dd30728cf",
   "blog":
-      "http://listeo.inspireui.com", //Your website woocommerce. You can remove this line if it same url
-  "forgetPassword": "http://demo.mstore.io/wp-login.php?action=lostpassword"
+      "http://blog.pariflorist.com", //Your website woocommerce. You can remove this line if it same url
+  "forgetPassword": "http://pariflorist.com/wp-login.php?action=lostpassword"
 };
-
 const afterShip = {
   "api": "e2e9bae8-ee39-46a9-a084-781d0139274f",
   "tracking_url": "https://fluxstore.aftership.com"
@@ -30,13 +29,17 @@ const ProductVariantLayout = {
   "height": "option",
 };
 
+
+/// This option is determine hide some components for web
+var kWebHidden = true;
+
 const kAdvanceConfig = {
   "DefaultLanguage": "en",
   "DefaultCurrency": {
-    "symbol": "\$",
+    "symbol": "\₹",
     "decimalDigits": 2,
     "symbolBeforeTheNumber": true,
-    "currency": "USD"
+    "currency": "IND"
   },
   "IsRequiredLogin": false,
   "GuestCheckout": true,
@@ -46,8 +49,8 @@ const kAdvanceConfig = {
   "GridCount": 3,
   "DetailedBlogLayout": kBlogLayout.halfSizeImageType,
   "EnablePointReward": true,
-  "DefaultPhoneISOCode": "+84",
-  "DefaultCountryISOCode": "VN",
+  "DefaultPhoneISOCode": "+91",
+  "DefaultCountryISOCode": "IN",
   "EnableRating": true,
   "EnableSmartChat": true,
   "hideOutOfStock": true,
@@ -57,18 +60,13 @@ const kAdvanceConfig = {
   "EnableConfigurableProduct": false, //for magento
   "EnableAttributesConfigurableProduct": ["color", "size"], //for magento
   "EnableAttributesLabelConfigurableProduct": ["color", "size"], //for magento,
+  "EnableAdvertisement": true,
   "Currencies": [
     {
-      "symbol": "\$",
+      "symbol": "\₹",
       "decimalDigits": 2,
       "symbolBeforeTheNumber": true,
-      "currency": "USD"
-    },
-    {
-      "symbol": "đ",
-      "decimalDigits": 2,
-      "symbolBeforeTheNumber": true,
-      "currency": "VND"
+      "currency": "IND"
     }
   ]
 };
@@ -78,7 +76,12 @@ const kAdvanceConfig = {
 /// The download package is remove these keys, please use your own key
 const kGoogleAPIKey = {
   "android": "your-google-api-key",
-  "ios": "your-google-api-key"
+  "ios": "your-google-api-key",
+  "web": "your-google-api-key"
+};
+
+const kOneSignalKey = {
+  'appID': "8b05d1a1-5485-4fd3-81c4-5f3276c9a7e8",
 };
 
 /// use to config the product image height for the product detail
@@ -97,27 +100,27 @@ const kProductDetail = {
 /// config for the chat app
 const smartChat = [
   {
-    'app': 'whatsapp://send?phone=84327433006',
+    'app': 'whatsapp://send?phone=+918497890956',
     'iconData': FontAwesomeIcons.whatsapp
   },
-  {'app': 'tel:8499999999', 'iconData': FontAwesomeIcons.phone},
-  {'app': 'sms://8499999999', 'iconData': FontAwesomeIcons.sms}
+  {'app': 'tel:8497890956', 'iconData': FontAwesomeIcons.phone},
+  {'app': 'sms://8497890956', 'iconData': FontAwesomeIcons.sms}
 ];
-const String adminEmail = "admininspireui@gmail.com";
+const String adminEmail = "support@pariflorist.com";
 
 /// the welcome screen data
 List onBoardingData = [
   {
-    "title": "Welcome to FluxStore",
-    "image": "assets/images/fogg-delivery-1.png",
-    "desc": "Fluxstore is on the way to serve you. "
+    "title": "Welcome to Pari Florist",
+    "image": "assets/images/parintrocrop.png",
+    "desc": "Pari is on the way to serve you. "
   },
   {
-    "title": "Connect Surrounding World",
-    "image": "assets/images/fogg-uploading-1.png",
+    "title": "We are delivering..",
+    "image": "assets/images/availcity.png",
     "desc":
-        "See all things happening around you just by a click in your phone. "
-            "Fast, convenient and clean."
+        "We only deliver to these cities."
+            "Any orders from other cities will not be accepted."
   },
   {
     "title": "Let's Get Started",
@@ -133,14 +136,13 @@ const PaypalConfig = {
       "ECbFREri7NFj64FI_9WzS6A0Az2DqNLrVokBo0ZBu4enHZKMKOvX45v9Y1NBPKFr6QJv2KaSp5vk5A1G",
   "production": false,
   "paymentMethodId": "paypal",
-  "enabled": true,
+  "enabled": false,
   "returnUrl": "http://return.example.com",
   "cancelUrl": "http://cancel.example.com",
 };
 
 const RazorpayConfig = {
-  "keyId": "rzp_test_WHBBYP8YoqmqwB",
-  "callbackUrl": "http://example.com",
+  "keyId": "rzp_test_r9yYHOfYXbmics",
   "paymentMethodId": "razorpay",
   "enabled": true
 };
@@ -153,7 +155,13 @@ const TapConfig = {
 };
 
 // Limit the country list from Billing Address
-const List DefaultCountry = [];
+const List DefaultCountry = [
+  {
+    "name": "India",
+    "iosCode": "IN",
+    "icon":
+        "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
+  }];
 //const List DefaultCountry = [
 //  {
 //    "name": "Vietnam",
@@ -168,3 +176,26 @@ const List DefaultCountry = [];
 //  },
 //  {"name": "Austria", "iosCode": "AT", "icon": ""},
 //];
+
+const kAdConfig = {
+  "enable": false,
+  "type": kAdType.facebookNative,
+  // ----------------- Facebook Ads  -------------- //
+  "hasdedIdTestingDevice": "ef9d4a6d-15fd-4893-981b-53d87a212c07",
+  "bannerPlacementId": "430258564493822_489007588618919",
+  "interstitialPlacementId": "430258564493822_489092398610438",
+  "nativePlacementId": "430258564493822_489092738610404",
+  "nativeBannerPlacementId": "430258564493822_489092925277052",
+
+  // ------------------ Google Admob  -------------- //
+  "androidAppId": "ca-app-pub-2101182411274198~6793075614",
+  "androidUnitBanner": "ca-app-pub-2101182411274198/4052745095",
+  "androidUnitInterstitial": "ca-app-pub-2101182411274198/7131168728",
+  "androidUnitReward": "ca-app-pub-2101182411274198/6939597036",
+  "iosAppId": "ca-app-pub-2101182411274198~6923444927",
+  "iosUnitBanner": "ca-app-pub-2101182411274198/5418791562",
+  "iosUnitInterstitial": "ca-app-pub-2101182411274198/9218413691",
+  "iosUnitReward": "ca-app-pub-2101182411274198/9026842008",
+  "waitingTimeToDisplayInterstitial": 10,
+  "waitingTimeToDisplayReward": 10,
+};
